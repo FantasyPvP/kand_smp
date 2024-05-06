1e2451511ee1eba2fee7549dfff27d043cf84213
Dan E <dhengland44@gmail.com>
2024-05-06 13:21:37 +0100

    Add messages for spawn tp, command added to change the spawn coordinates.

--- src/main/java/fantasypvp/kand_smp/Kand_smp.java
+++ src/main/java/fantasypvp/kand_smp/Kand_smp.java
@@ -2,11 +2,12 @@ package fantasypvp.kand_smp;
 
 import fantasypvp.kand_smp.commands.CmdLightningSword;
 import fantasypvp.kand_smp.commands.CmdTeleportSpawn;
+import fantasypvp.kand_smp.commands.SetSpawnCommand;
 import fantasypvp.kand_smp.events.Events;
 import fantasypvp.kand_smp.items.LightningGear;
 import fantasypvp.kand_smp.items.TrueNetherite;
 import fantasypvp.kand_smp.items.Witherite;
-import me.danthepythonman.spawntp.commands.SetSpawnCommand;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Kand_smp extends JavaPlugin {
